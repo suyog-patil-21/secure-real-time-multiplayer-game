b554cb276f3801279f190e2376dcecccb0f3cda5
Suyog Patil <suyog0057@gmail.com>
2024-05-19 11:59:57 +0530

    add: added functios of collectible and Player

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,6 +1,9 @@
 class Collectible {
-  constructor({x, y, value, id}) {
-
+  constructor({ x, y, value, id }) {
+    this.x = x;
+    this.y = y;
+    this.id = id;
+    this.value = value;
   }
 
 }
@@ -11,6 +14,6 @@ class Collectible {
 */
 try {
   module.exports = Collectible;
-} catch(e) {}
+} catch (e) { }
 
 export default Collectible;
--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,40 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor({ x, y, score, id }) {
+    this.id = id;
+    this.score = score;
+    this.x = x;
+    this.y = y;
   }
 
   movePlayer(dir, speed) {
-
+    if (dir == 'up') {
+      this.y = this.y + speed;
+    }
+    if (dir == 'down') {
+      this.y = this.y + speed;
+    }
+    if (dir == 'left') {
+      this.x = this.x - speed;
+    }
+    if (dir == 'right') {
+      this.x = this.x + speed;
+    }
   }
 
   collision(item) {
-
+    if (this.x == item.x && this.y == item.y) return true;
+    return false;
   }
 
   calculateRank(arr) {
-
+    const totalPlayers = arr.length;
+    arr.sort(function (a, b) { return b.score - a.score });
+    let currentRanking = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id == this.id)
+        currentRanking = i + 1;
+    }
+    return `Rank: ${currentRanking}/${totalPlayers}`;
   }
 }
 
